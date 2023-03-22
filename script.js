1ea4c693554b9a52b4ca1e5a51e401b03473a66f
aznyan <aznyan5510@gmail.com>
2023-03-21 23:18:14 -0400

    Change gridSize to 256 to have it 16 x 16

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 const grid = document.querySelector('.grid');
 
 // creating grids
-let gridSize = 16;
+let gridSize = 256;
 
 for (let i = 0; i < gridSize; i++) {
     let tag = document.createElement('div');
     tag.id = i;
     tag.className = 'gridbox';
-    tag.style.backgroundColor = 'gray';
+    tag.style.backgroundColor = 'black';
     grid.append(tag);
 }
