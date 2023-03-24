d05b4e72c4d57c596e60fdecd5f36f34da362f3d
aznyan <aznyan5510@gmail.com>
2023-03-24 00:55:22 -0400

    Add eventlistner to the grid when mouse hover it and added a hover class to it

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// Different colors to choose from
+let colorTheme = ['black', 'red', 'blue'];
+
 // Necessary elements 
 const grid = document.querySelector('.grid');
+let tag = document.createElement('div');
 
 // creating grids
 let gridSize = 256;
@@ -8,6 +12,9 @@ for (let i = 0; i < gridSize; i++) {
     let tag = document.createElement('div');
     tag.id = i;
     tag.className = 'gridbox';
-    tag.style.backgroundColor = 'black';
-    grid.append(tag);
+    grid.appendChild(tag);
+
+    tag.addEventListener('mouseover', function(){
+        tag.classList.add('hover');
+    });
 }
