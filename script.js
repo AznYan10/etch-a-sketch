58d15c46b7840e41a198925ed9b1b946ebd60e03
aznyan <aznyan5510@gmail.com>
2023-03-28 19:11:58 -0400

    Created grid function when making a grid and when button is clicked it will remove and replace the grid pixesl with the users input

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-// Different colors to choose from
-let colorTheme = ['black', 'red', 'blue'];
-
 // Necessary elements 
+const changePixBtn = document.querySelector('#changePix');
+const mainGrid = document.querySelector('.mainGrid');
 const grid = document.querySelector('.grid');
-let tag = document.createElement('div');
+let isCreate = true;
 
 // creating grids
 let gridSize = 256;
+let newGrid = 40;
 
-for (let i = 0; i < gridSize; i++) {
-    let tag = document.createElement('div');
-    tag.id = i;
-    tag.className = 'gridbox';
-    grid.appendChild(tag);
-
-    tag.addEventListener('mouseover', function(){
-        tag.classList.add('hover');
-    });
+function createGrid () {
+    for (let i = 0; i < gridSize; i++) {
+        if(isCreate == false) {
+            while (grid.firstChild) {
+                grid.removeChild(grid.firstChild);
+            }
+            isCreate = true;
+        } else {
+            let tag = document.createElement('div');
+            tag.id = i;
+            tag.style.width = newGrid + 'px';
+            tag.style.height = newGrid + 'px';
+            tag.style.backgroundColor = 'white';
+            tag.style.color = 'white';
+            grid.appendChild(tag);
+            // Event listener when mouse over in the grid
+            tag.addEventListener('mouseover', function(){
+                tag.style.position = 'relative';
+                tag.style.backgroundColor = 'black';
+                console.log(tag);
+            });
+        }
+    }
 }
+
+// When change pixel is clicked change grid size
+function changePixFunc () {
+    isCreate = false;
+    newGrid = prompt('Enter new grid size (1 - 100): ');
+    if (newGrid > 0 && newGrid <= 100) {
+        gridSize = newGrid * newGrid + 1;
+        newGrid = (640 / newGrid);
+        createGrid();
+    } else {
+        alert("Incorrect Input!");
+    }
+};
+
+// calling functions 
+changePixBtn.addEventListener('click', changePixFunc);
+createGrid();
